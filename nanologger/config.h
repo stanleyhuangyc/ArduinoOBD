881ba1dec955faaf052e55259d5f315e77781934
Stanley Huang <stanleyhuangyc@gmail.com>
2014-08-15 21:23:06 +1000

    Update Nano Logger

--- nanologger/config.h
+++ nanologger/config.h
@@ -11,25 +11,12 @@
 #define LOG_FORMAT FORMAT_CSV
 
 /**************************************
-* Default working mode
-**************************************/
-#define MODE_DEFAULT MODE_LOGGER /* MODE_LOGGER/MODE_TIMER */
-//#define MODE_SWITCH_PIN 8
-
-/**************************************
 * Choose SD pin here
 **************************************/
 //#define SD_CS_PIN SS // generic
 //#define SD_CS_PIN 4 // ethernet shield
 //#define SD_CS_PIN 7 // microduino
 #define SD_CS_PIN 10 // SD breakout
-
-/**************************************
-* Config GPS here
-**************************************/
-#define USE_GPS 0
-#define GPS_BAUDRATE 38400 /* bps */
-//#define GPS_OPEN_BAUDRATE 4800 /* bps */
 
 /**************************************
 * Choose LCD model here
@@ -41,9 +28,6 @@ LCD_SH1106 lcd;
 /**************************************
 * Other options
 **************************************/
-#define USE_MPU6050 0
-//#define OBD_MIN_INTERVAL 50 /* ms */
-#define GPS_DATA_TIMEOUT 2000 /* ms */
 //#define DEBUG Serial
 #define DEBUG_BAUDRATE 9600
 
