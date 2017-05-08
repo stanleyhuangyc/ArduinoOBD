299de0a90617cb3a0a852aad1df538d1e23272b1
Stanley Huang <stanleyhuangyc@gmail.com>
2017-05-08 22:51:25 +1000

    Reference data logger sketch for Freematics OBD-II UART Adapter

--- datalogger/config.h
+++ datalogger/config.h
@@ -14,11 +14,8 @@
 #define ENABLE_DATA_CACHE 0
 #define MAX_CACHE_SIZE 256  /* bytes */
 
-// followings define the format of data streaming, enable one of them only
-// FORMAT_BIN is required by Freematics OBD iOS App
-//#define STREAM_FORMAT FORMAT_BIN
-// FORMAT_TEXT is for readable text output
 #define STREAM_FORMAT FORMAT_TEXT
+//#define STREAM_FORMAT FORMAT_BIN
 
 // serial baudrate for data out streaming
 #define STREAM_BAUDRATE 115200
@@ -33,9 +30,6 @@
 // max allowed time for connecting OBD-II (0 for forever)
 #define OBD_ATTEMPT_TIME 0 /* seconds */
 
-// OBD-II UART baudrate
-#define OBD_UART_BAUDRATE 115200L
-
 // SD pin
 #define SD_CS_PIN 10
 
@@ -46,9 +40,7 @@
 #define COMPASS_DATA_RATIO 8
 
 // enable(1)/disable(0) GPS module
-#define USE_GPS 1
-#define LOG_GPS_NMEA_DATA 0
-#define LOG_GPS_PARSED_DATA 1
+#define USE_GPS 0
 
 // GPS parameters
 #define GPS_SERIAL_BAUDRATE 115200L
